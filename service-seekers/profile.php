8e39a6bb9273de768f6424740633d58ed978a988
Mushkir <mushkirmohamed@gmail.com>
2024-04-27 15:26:29 +0530

    style: designed SS profile ui

--- service-seekers/profile.php
+++ service-seekers/profile.php
@@ -1,1 +1,50 @@
-<?php echo "From Profile";
+<main class="">
+    <section class=" sm:mt-20 px-3 sm:px-5 md:px-20">
+        <div class=" bg-gray-400 w-full rounded-xl mx-auto p-10" id="ss-profile-card">
+            <div class="mx-auto flex w-[200px] justify-center items-center bg-[#6D2932] rounded-full p-1">
+                <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmpbzPe1HdyFoLu4qK_e12GtP3sJxMeVWexNvzFsPB_A&s" class="w-[200px] h-[200px] object-cover rounded-full" alt="'s Picture." />
+            </div>
+            <div class="md:flex gap-1 items-start">
+                <div class="mt-5">
+                    <h5 class="text-center text-xl text-[#6D2932] md:text-left font-semibold">
+                        Mohamed Mushkir
+                    </h5>
+                    <p class="card-text text-center md:text-left mt-3">
+                        <!-- No. 751 East Milton Drive, Kandy, Sri Lanka -->
+                        📍 No. 751 East Milton Drive, Kandy, Sri Lanka
+                    </p>
+                </div>
+            </div>
+
+            <!-- Email -->
+            <div class="text-[12px] mt-4 mb-4 flex justify-between bg-gray-100 py-2 rounded-lg">
+                <span class="px-1.5">Email: </span>
+                <span class="px-1.5">mushkirmohamed@gmail.com</span>
+            </div>
+
+            <!-- Username -->
+            <div class="text-[12px] mb-4 flex justify-between bg-gray-100 py-2 rounded-lg">
+                <span class="px-1.5">Username: </span>
+                <span class="px-1.5">mushkir_96</span>
+            </div>
+
+            <!-- Username -->
+            <div class="text-[12px] mb-4 flex justify-between bg-gray-100 py-2 rounded-lg">
+                <span class="px-1.5">Gender: </span>
+                <span class="px-1.5">Male</span>
+            </div>
+
+            <!-- Contact No -->
+            <div class="text-[12px] mt-4 mb-3 flex justify-between bg-gray-100 py-2 rounded-lg">
+                <span class="px-1.5">Contact No: </span>
+                <span class="px-1.5">+94777195282</span>
+            </div>
+
+            <!-- NIC -->
+            <div class="text-[12px] mb-4 flex justify-between bg-gray-100 py-2 rounded-lg">
+                <span class="px-1.5">Identity Card No: </span>
+                <span class="px-1.5">199631401505</span>
+            </div>
+        </div>
+    </section>
+</main>
